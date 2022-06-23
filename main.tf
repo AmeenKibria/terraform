c7a982276b4602efaac24a728671bc09ee7c3c54
AmeenKibria <72705976+AmeenKibria@users.noreply.github.com>
2022-06-23 12:30:19 +0300

    added vpc along with an instance

--- main.tf
+++ main.tf
@@ -2,27 +2,49 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-08e93a9522bbe6df6"
   instance_type = var.instance_type
 
+  network_interface {
+    network_interface_id = aws_network_interface.nic.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
   tags = {
-    Name = var.instance_name
+    Name    = "tf-cloud-learning"
+    tooling = "tf-cloud"
+  }
+}
+
+resource "aws_vpc" "tf_cloud_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name    = "tf-cloud-learning"
+    tooling = "tf-cloud"
+  }
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.tf_cloud_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name    = "tf-cloud-learning"
+    tooling = "tf-cloud"
+  }
+}
+
+resource "aws_network_interface" "nic" {
+  subnet_id   = aws_subnet.private_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name    = "tf-cloud-learning"
+    tooling = "tf-cloud"
   }
 }
